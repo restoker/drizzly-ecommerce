3d3e96faf24d53d340cb9a36589b19cf072a4274
restoker <milthon.doom@gmail.com>
2024-10-09 11:40:28 -0500

    update variant product added

--- server/actions/create-product-variant.ts
+++ server/actions/create-product-variant.ts
@@ -3,6 +3,7 @@ import { actionClient } from "@/types/safe-action";
 import { db } from "..";
 import { productVariant, variantImages, variantTags } from "../schema";
 import { eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
 export const createProductVariant = actionClient
     .schema(productVariantSchema)
@@ -14,23 +15,35 @@ export const createProductVariant = actionClient
             });
             console.log(product);
             if (!product) return { ok: false, msg: `Product with id:${productId} not found` };
-            // if (editMode && id) {
-            //     // const productUpdate = Object.assign(product, )
-            //     const editVariant = await db
-            //         .update(productVariant)
-            //         .set({ color, productType, updated: new Date() })
-            //         .where(eq(productVariant.id, id))
-            //         .returning();
-            //     console.log(editVariant);
-            //     await db.delete(variantTags).where(eq(variantTags.id, editVariant[0].id));
-            //     await db.insert(variantTags).values(
-            //         tags.map((tag) => ({
-            //             tag,
-            //             variantId: editVariant[0].id,
-            //         }))
-            //     );
-            //     await db.delete(variantImages).where(eq(variantImages.variantId, editVariant[0].id))
-            // }
+            if (editMode && id) {
+                // const productUpdate = Object.assign(product, )
+                const editVariant = await db
+                    .update(productVariant)
+                    .set({ color, productType, updated: new Date() })
+                    .where(eq(productVariant.id, id))
+                    .returning();
+                console.log(editVariant);
+                await db.delete(variantTags).where(eq(variantTags.id, editVariant[0].id));
+                await db.insert(variantTags).values(
+                    tags.map((tag) => ({
+                        tag,
+                        variantId: editVariant[0].id,
+                    }))
+                );
+                await db.delete(variantImages).where(eq(variantImages.variantId, editVariant[0].id));
+                db.insert(variantImages).values(
+                    newImages.map((image, i) => ({
+                        name: image.name,
+                        size: image.size,
+                        url: image.url,
+                        variantId: editVariant[0].id,
+                        order: i,
+                    }))
+                );
+
+                revalidatePath('/dashboard/products');
+                return { ok: true, msg: 'Product was updated' };
+            }
         } catch (error) {
             return {
                 ok: false,
